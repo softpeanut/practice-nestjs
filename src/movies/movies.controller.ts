14613026fadbe749b77df35b9a44e18465fea97f
김범진 <sdpthf@naver.com>
2021-12-28 00:13:22 +0900

    ⚡ :: 2.1 more routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
@@ -9,6 +9,11 @@ export class MoviesController {
     getAll() {
         return "This will return all movies";
     }
+
+    @Get("search")
+    search(@Query("name") movieTitle: string) {
+        return `We are searching for a movie with a title: ${movieTitle}`;
+    }
     
     @Get("/:id")
     getOne(@Param("id") id: string) {
@@ -18,6 +23,19 @@ export class MoviesController {
     @Post()
     postOne(@Body() name: string) {
         return name;
+    }
+
+    @Patch("/:id")
+    patch(@Param("id") movieId: string, @Body() movieData) {
+        return {
+            movie_id: movieId,
+            ...movieData
+        };
+    }
+
+    @Delete("/:id")
+    delete(@Param("id") movidId: string) {
+        return movidId;
     }
 
 }
