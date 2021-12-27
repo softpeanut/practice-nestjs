59c0d7a6d159b96368e0de566d68d0b5a6b7c0cf
김범진 <sdpthf@naver.com>
2021-12-28 00:00:00 +0900

    ⚡ :: 2.0 movies controller

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
     constructor(private readonly movieservice: MoviesService) {}
+
+    @Get()
+    getAll() {
+        return "This will return all movies";
+    }
+    
+    @Get("/:id")
+    getOne(@Param("id") id: string) {
+        return id;
+    }
+
+    @Post()
+    postOne(@Body() name: string) {
+        return name;
+    }
+
 }
