893b3f9619ef176c9e93335be79027dfacab31eb
김범진 <sdpthf@naver.com>
2021-12-28 11:21:00 +0900

    ⚡ :: 2.5 DTOs and Validation part Two

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNumber } from 'class-validator';
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateMovieDto {
 
@@ -8,6 +8,7 @@ export class CreateMovieDto {
     @IsNumber()
     readonly year: number;
 
+    @IsOptional()
     @IsString({ each: true })
     readonly genres: string[];
     
--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,11 +1,4 @@
-import { IsString, IsNumber } from 'class-validator';
+import {PartialType} from '@nestjs/mapped-types';
+import { CreateMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto {
-    
-    @IsString()
-    title: string;
-
-    @IsNumber()
-    year: number;
-    
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
